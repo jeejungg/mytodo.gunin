4047c6e1748ceb37453d29915922a8e0581aad66
witawas leesuksomboon <witawassgunn@gmail.com>
2019-01-16 11:19:11 +0700

    add nmp i mongo&connect DB mongo

--- server.js
+++ server.js
@@ -2,6 +2,17 @@
 
 const Hapi=require('hapi');
 
+
+    const dbOpts = {
+        url: 'mongodb://admin:admin@cluster0-shard-00-00-sdzlc.gcp.mongodb.net:27017,cluster0-shard-00-01-sdzlc.gcp.mongodb.net:27017,cluster0-shard-00-02-sdzlc.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+        settings: {
+            poolSize: 10
+        },
+        decorate: true
+    };
+
+   
+
 // Create a server with a host and port
 const server = Hapi.server({
     host:'0.0.0.0',
@@ -23,6 +34,10 @@ const start =  async function() {
 
     try {
         await server.start();
+        await server.register({
+            plugin: require('hapi-mongodb'),
+            options: dbOpts
+        });
     }
     catch (err) {
         console.log(err);
