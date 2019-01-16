205ae43b76bba54db6447b7fed419819a0aeefc1
jeejung <infinite.ii@live.com>
2019-01-16 10:58:03 +0700

    addroute

--- server.js
+++ server.js
@@ -3,15 +3,15 @@
 const Hapi=require('hapi');
 
 // Create a server with a host and port
-const server=Hapi.server({
-    host:'localhost',
-    port:8000
-});
+const server = Hapi.server({
+    host:'0.0.0.0',
+    port: process.env.PORT|| 8000
+ });
 
 // Add the route
 server.route({
     method:'GET',
-    path:'/hello',
+    path:'/',
     handler:function(request,h) {
 
         return'hello world';
